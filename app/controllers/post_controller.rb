6c28b4531e777c31b082bd8ad0746bf5390bb06c
youngthur <youngthur@naver.com>
2018-05-14 11:52:12 +0000

    isthattrue

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,17 @@
 class PostController < ApplicationController
+    def home
+        @post = Post.new
+    end
+    
+    def index
+    end
+    
+    def create
+        @post = Post.new
+        @post.title = params[:name_info]
+        @post.content = params[:price_info]
+        
+        redirect_to "/"
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  
+  root "post#home"
+  
+  post '/post/create' => 'post#create', as: 'post_create'
+  
+    get '/post/index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
